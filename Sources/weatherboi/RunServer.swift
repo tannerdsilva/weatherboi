4eb8e552f10c2ae5e18a3f9523428afd88b2c7c9
tannerdsilva <t@tannersilva.com>
2025-07-06 19:21:34 -0500

    revised log level, I need to make this configurable from the cli

--- Sources/weatherboi/RunServer.swift
+++ Sources/weatherboi/RunServer.swift
@@ -27,10 +27,10 @@ extension CLI {
 		func run() async throws {
 			let homeDirectory = Path(databasePath)
 			let eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-			let metadataDB = try MetadataDB(base:homeDirectory, logLevel:.trace)
-			let rainDB = try RainDB(base:homeDirectory, logLevel:.trace)
-			let mainDB = try WxDB(base:homeDirectory, logLevel:.trace)
-			let server = try HTTPServer(eventLoopGroupProvider:.shared(eventLoopGroup), bindV4:bindV4, bindV6:bindV6, port:Int(port), metadataDB:metadataDB, rainDB:rainDB, wxDB:mainDB, logLevel:.trace)
+			let metadataDB = try MetadataDB(base:homeDirectory, logLevel:.debug)
+			let rainDB = try RainDB(base:homeDirectory, logLevel:.debug)
+			let mainDB = try WxDB(base:homeDirectory, logLevel:.debug)
+			let server = try HTTPServer(eventLoopGroupProvider:.shared(eventLoopGroup), bindV4:bindV4, bindV6:bindV6, port:Int(port), metadataDB:metadataDB, rainDB:rainDB, wxDB:mainDB, logLevel:.debug)
 			try await ServiceGroup(services:[server], gracefulShutdownSignals:[.sigterm, .sigint], logger:Logger(label:"weatherboi.server")).run()
 		}
 	}
